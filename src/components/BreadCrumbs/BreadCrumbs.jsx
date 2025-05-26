e2dedff90829937a0b94b3171d327ffeec49170b
Emine Avşar <mahinurk3438@gmail.com>
2025-05-26 13:27:00 +0300

    [MA-19] - changes added

--- src/components/BreadCrumbs/BreadCrumbs.jsx
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import styles from "./BreadCrumbs.module.scss";
+import { Link, useLocation, useResolvedPath } from "react-router";
 
 const Breadcrumb = () => {
-  const path = window.location.pathname;
+
+  const location = useLocation();
+  const resolved = useResolvedPath(location.pathname);
+  const path = resolved.pathname;
   const segments = path.split("/").filter(Boolean);
-  const last = segments[segments.length - 1] || "home";
+  const last = segments[segments.length -1] || "home";
+  
 
   return (
     <nav className={styles.breadcrumb}>
-      <a href="/" className={styles.link}>Home</a>
+      <Link to="/" className={styles.link}>Home</Link>
       {last !== "home" && (
         <>
-          <span className={styles.separator}>/</span>
-          <span className={styles.current}>
-            {decodeURIComponent(last)
-              .replace(/-/g, " ")
-              .replace(/\b\w/g, (l) => l.toUpperCase())}
-          </span>
+        <span className={styles.separator}> / </span>
+        <span className={styles.current}>
+          {decodeURIComponent(last)
+          .replace(/-/g, " ")
+          .replace(/\b\w/g, (l) => l.toUpperCase())}
+        </span>
         </>
       )}
     </nav>
