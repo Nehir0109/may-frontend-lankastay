ec40a873cb9b9db6ae0a0d52c9a7ff3ca0f97048
palaogluserdest <palaogluserdest@gmail.com>
2025-05-31 20:46:27 +0300

    [MA-19] - Cleaned the unnecessary import and moved Breadcrump component to ui folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import "./styles/global.scss";
-import DetailsPage from "./components/Details/DetailsPage";
 
 // React Router route yapısının kurgulanacağı kısımdır.
 //  Detaylı bilgi için https://reactrouter.com/start/data/installation
