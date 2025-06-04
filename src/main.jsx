3e815dd051c945f7551b585ecdd2bd574a146d11
Nihat <nihatt.nihatt2002@gmail.com>
2025-06-04 15:57:28 +0400

    [MA-26] - modal component created

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import "./styles/global.scss";
+import VerifiedScreen from "./components/VerifiedScreen/VerifiedScreen";
 
 // React Router route yapısının kurgulanacağı kısımdır.
 //  Detaylı bilgi için https://reactrouter.com/start/data/installation
@@ -11,10 +11,18 @@ const router = createBrowserRouter([
     path: "/",
     Component: Home,
   },
+    {
+     path: "/verified",
+     Component: VerifiedScreen,
+    },
+    {
+        path: "/dashboard",
+        Component: () => <h1>Dashboard</h1>,
+    }
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <RouterProvider router={router} />
-  </StrictMode>
+  </>
 );
