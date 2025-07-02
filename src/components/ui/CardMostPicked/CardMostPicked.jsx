cfa72835baebbbcd49829a3b68607a6a235dc7d1
yzkn07 <mehmetali.1992@hotmail.com>
2025-07-02 20:23:10 +0300

    [MA-6] - most picked new

--- src/components/ui/CardMostPicked/CardMostPicked.jsx
+++ src/components/ui/CardMostPicked/CardMostPicked.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import useData from "../../../hooks/useData";
 import MostPickedCard from "../MostPickedCard/MostPickedCard";
 import styles from "./CardMostPicked.module.scss";
