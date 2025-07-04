bb6ce506bf70bfb87b01c850d4f3d66e00904940
Nehir0109 <nehir.cakilci@gmail.com>
2025-07-04 12:21:34 +0300

    [MA-03] - header re-implemented with NavLink component

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,28 +1,47 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router";
 import Button from "../../ui/Button/Button.jsx";
 import styles from "./Navbar.module.scss";
 import logo from "@/assets/logo.svg"; 
 
 const Navbar = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const loggedIn = localStorage.getItem("isLoggedIn") === "true";
+        setIsLoggedIn(loggedIn);
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("isLoggedIn");
+        setIsLoggedIn(false);
+        navigate("/"); // Anasayfaya yönlendir
+    };
 
     return (
         <nav className={styles.navbar}>
-            <div className={styles.logo} onClick={() => navigate("/")}>
-                <img src={logo} alt="Logo" />
+            <div className={styles.logo}>
+                <NavLink to="/"> <img src={logo} alt="Logo"/> </NavLink>
             </div>
 
             <div className={styles.navbarContent}>
                 <ul className={styles.navLinks}>
-                    <li onClick={() => navigate("/")}>Home</li>
-                    <li onClick={() => navigate("/hotels")}>Hotels</li>
-                    <li onClick={() => navigate("/rooms")}>Rooms</li>
-                    <li onClick={() => navigate("/about")}>About</li>
-                    <li onClick={() => navigate("/contact")}>Contact</li>
+                    <li> <NavLink to="/" className={({ isActive }) => isActive ? styles.navActive : undefined} >Home</NavLink> </li>
+                    <li> <NavLink to="/hotels" className={({ isActive }) => isActive ? styles.navActive : undefined} >Hotels</NavLink> </li>
+                    <li> <NavLink to="/rooms" className={({ isActive }) => isActive ? styles.navActive : undefined} >Rooms</NavLink> </li>
+                    <li> <NavLink to="/about" className={({ isActive }) => isActive ? styles.navActive : undefined} >About</NavLink> </li>
+                    <li> <NavLink to="/contact" className={({ isActive }) => isActive ? styles.navActive : undefined} >Contact</NavLink> </li>
                 </ul>
 
                 <div>
-                    <Button className={styles.loginBtn} onClick={() => navigate("/login")}>Login</Button>
+                    {isLoggedIn ? (
+                        <Button className={styles.loginBtn} onClick={handleLogout}>Logout</Button>
+                    ) : (
+                        <Button className={styles.loginBtn}>
+                            <NavLink to="/login">Login</NavLink>
+                        </Button>
+                    )}
                 </div>
 
             </div>
