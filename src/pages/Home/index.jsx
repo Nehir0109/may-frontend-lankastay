3c484c460566f4623ea78ae33fa598936241dc66
palaogluserdest <palaogluserdest@gmail.com>
2025-08-02 20:14:22 +0300

    [JU-000] - Removed traesure component from home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,7 @@
-import TreasureToChoose from "../../components/TreasureChoose/TreasureToChoose.jsx";
-import useData from "../../hooks/useData.js";
-
-
 const Home = () => {
-
-    const {data} = useData("hotels");
-
   return (
     <>
       <h1>Home Page</h1>
-        <TreasureToChoose
-        choices={data ? data : []}
-        />
     </>
   );
 };
