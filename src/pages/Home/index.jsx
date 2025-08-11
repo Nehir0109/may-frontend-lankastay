1f86b73a4a5f87141f56cb4cf911490cf72ad6dc
Emine Avşar <mahinurk3438@gmail.com>
2025-08-11 21:52:42 +0300

    [MA-8] - footer added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import HeroSection from "../../components/HeroSection/HeroSection";
 import SearchFilterBar from "../../components/SearchFilterBar";
 import Navbar from "../../components/Header/Navbar/Navbar.jsx";
 import Register from "../../components/Register/Register.jsx";
+import Footer from "../../components/Footer/Footer.jsx" ;
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <header><Navbar /></header>
       <HeroSection />
       <SearchFilterBar />
+      <Footer/>
       
     </>
   );
