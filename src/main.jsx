71ac963f9e9e5245c5e77c5869529b41ef58ff84
Nehir0109 <nehir.cakilci@gmail.com>
2025-08-12 10:02:58 +0300

    [MA-36] - Create BookingPage with product data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import "./styles/global.scss";
 import VerifiedScreen from "./components/VerifiedScreen/VerifiedScreen";
+import BookingPage from "./pages/BookingPage/Booking";
 
 // React Router route yapısının kurgulanacağı kısımdır.
 //  Detaylı bilgi için https://reactrouter.com/start/data/installation
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+  },
+  {
+    path: "/booking/:id",
+    Component: BookingPage,
   },
 ]);
 
---
+++ src/pages/BookingPage/Booking.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import BookingCriteria from "../../components/BookingCriteria/BookingCriteria.jsx";
+
+function BookingPage() {
+const { id } = useParams(); 
+const [product, setProduct] = useState(null);
+
+useEffect(() => {
+// ID parametresine göre veri çekme
+fetch(`https://6829efb0ab2b5004cb3543e3.mockapi.io/api/v1/${id}`)
+.then((response) => response.json())
+.then((data) => setProduct(data));
+}, [id]);
+
+if (!product) return <p>Loading...</p>;
+
+return (
+<div>
+<BookingCriteria onChange={(data) => setBookingData(data)} />
+</div>
+);
+}
+
+export default BookingPage;
