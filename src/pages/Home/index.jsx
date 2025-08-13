9c958578aff9f27d917adcb9e554e9523d62d6d2
palaogluserdest <palaogluserdest@gmail.com>
2025-08-13 20:36:05 +0300

    [MA-7] - completed section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,19 @@
-
 import HeroSection from "../../components/HeroSection/HeroSection";
 import SearchFilterBar from "../../components/SearchFilterBar";
 import Navbar from "../../components/Header/Navbar/Navbar.jsx";
 import Register from "../../components/Register/Register.jsx";
-import Footer from "../../components/Footer/Footer.jsx" ;
+import Footer from "../../components/Footer/Footer.jsx";
 import PopularChoiceSection from "../../components/PopularChoiceSection";
 const Home = () => {
   return (
     <>
-      <header><Navbar /></header>
+      <header>
+        <Navbar />
+      </header>
       <HeroSection />
       <SearchFilterBar />
-      <Footer/>
       <PopularChoiceSection />
+      <Footer />
     </>
   );
 };
