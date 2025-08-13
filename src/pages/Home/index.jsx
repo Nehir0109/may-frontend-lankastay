9659e725cb443ade9637ecd6b1c70d21a3d2e4bf
palaogluserdest <palaogluserdest@gmail.com>
2025-08-13 20:33:59 +0300

    [MA-23] - Completed booking price section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,20 @@
-
 import HeroSection from "../../components/HeroSection/HeroSection";
 import SearchFilterBar from "../../components/SearchFilterBar";
 import Navbar from "../../components/Header/Navbar/Navbar.jsx";
-import Register from "../../components/Register/Register.jsx";
-import Footer from "../../components/Footer/Footer.jsx" ;
-import Booking from "../../components/Booking"; // ✅ sadece klasör adı
+import Footer from "../../components/Footer/Footer.jsx";
 
 const Home = () => {
   return (
     <>
-      <header><Navbar /></header>
+      <header>
+        <Navbar />
+      </header>
       <HeroSection />
       <SearchFilterBar />
-      <Footer/>
-       <div style={{ padding: "40px" }}>
-      <SearchFilterBar />
-      <Booking price="$200" />
-    </div>
+      <Footer />
+      <div style={{ padding: "40px" }}>
+        <SearchFilterBar />
+      </div>
     </>
   );
 };
